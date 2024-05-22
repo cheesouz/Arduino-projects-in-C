6a58ebbff1cb610b26bc1900d56586babab2e772
lucy.poplawska <lucy.poplawska@student.kdg.be>
2024-05-22 17:47:29 +0200

    Can display and move both trex and arrows

--- Project/src/main.c
+++ Project/src/main.c
@@ -5,6 +5,7 @@
 #include <util/delay.h>
 #include <button.h>
 #include <stdlib.h>
+#include <led.h>
 
 #define BUTTON_PORT PORTC
 #define BUTTON_PIN PINC
@@ -12,33 +13,47 @@
 #define BUTTON1 PC1
 #define BUTTON3 PC3
 
-//how long it takes the arrow to reach the dinosaur
-#define ARROW_SPEED 9
+
+static int counter = 0;
+static int dinoPos = 1;
+static int dinoMoveFlag = 1;
+const int gameSpeed = 400;
 
 /* Segment byte maps for dino's positions: up, normal, down */
 const uint8_t DINO_POS[] =  {0x9C, 0xA3, 0xF7};
 
 /* Segment byte maps for arrow's positions */
-const uint8_t ARROW_POS[] =  {0xFE, 0xBF, 0xF7};
+const uint8_t ARROW_HEIGHT[] =  {0xFE, 0xBF, 0xF7};
 
 /* Byte maps to select digit 1 to 4 */
 const uint8_t SEGMENTS[] = {0xF1, 0xF2, 0xF4, 0xF8};
 
-void moveArrow(uint8_t segment, uint8_t height) {
-  cbi(PORTD, LATCH_DIO);
-  shift(height, MSBFIRST);
-  shift(SEGMENTS[segment], MSBFIRST);
-  sbi(PORTD, LATCH_DIO);
+void initTimer0()
+{
+    // STEP 1: choose the WAVE FORM and by consequence the Mode of Operation
+    // We choose FAST PWM; TCNT0 counts to 255
+    TCCR0A |= _BV(WGM00) | _BV(WGM01); // WGM00 = 1 and WGM01 = 1 --> Fast PWM Mode
+
+    // STEP 2: *always* set a PRESCALER, otherwise the timer won't count
+    // The counting speed is determined by the CPU clock (16 Mhz) divided by this factor
+    TCCR0B |= _BV(CS01) | _BV(CS00); // CS00 and CS01 = 1 --> prescaler, factor is now 64
+    // STEP 3: enable INTERRUPTS
+    // Enable interrupts for overflow: TCNT0 == TOP
+    TIMSK0 |= _BV(TOIE0); // enable overflow interrupt
+}
+void clearDisplay(){
+    cbi(PORTD, LATCH_DIO);
+    shift(0x00, MSBFIRST); 
+    shift(0x00, MSBFIRST); 
+    sbi(PORTD, LATCH_DIO);
 }
 
-void shootArrow(){ 
-  srand(0);
-  uint8_t arrowHeight = ARROW_POS[rand() %3];
-  for (int i = 3; i > 0; i--){
-    moveArrow(i, arrowHeight);
-    delay(ARROW_SPEED/3);
-  }
-
+void displayArrow(uint8_t segment, uint8_t height) {
+  clearDisplay();
+  cbi(PORTD, LATCH_DIO);
+  shift(height, MSBFIRST);
+  shift(SEGMENTS[segment-1], MSBFIRST);
+  sbi(PORTD, LATCH_DIO);
 }
 
 void setupButtons(){
@@ -52,7 +67,8 @@ void setupButtons(){
   PCMSK1 |= _BV( BUTTON3 );
 }
 
-void changeDinoPos(uint8_t value) {
+void displayDino(uint8_t value) {
+  clearDisplay();
   cbi(PORTD, LATCH_DIO);
   shift(DINO_POS[value], MSBFIRST);
   shift(0xF1, MSBFIRST);
@@ -66,9 +82,7 @@ ISR( PCINT1_vect ){
     if (bit_is_clear(BUTTON_PIN, BUTTON1)){
       printf("Button 1 pressed in interrupt\n");
       //ToDo: Check if dino is in a neutral position
-      changeDinoPos(0);
-      _delay_ms(400);
-      changeDinoPos(1);
+      if (dinoMoveFlag == 1){ dinoMoveFlag = 0; }
   }
   }
   if (bit_is_clear(BUTTON_PIN, BUTTON3)){
@@ -76,27 +90,84 @@ ISR( PCINT1_vect ){
     _delay_ms( 50 );
     if (bit_is_clear(BUTTON_PIN, BUTTON3)){
       printf("Button 2 pressed in interrupt\n");
-      //ToDo: Check if dino is in a neutral position
-      changeDinoPos(2);
-      _delay_ms(400);
-      changeDinoPos(1);
+      if (dinoMoveFlag == 1){ dinoMoveFlag = 2; }
   }
 
   }
 
 }
 
+ISR(TIMER0_OVF_vect) {
+    static uint8_t currentSegment = 4;
+    static uint8_t arrowHeight = 1; // Position index
+    static uint8_t arrowPosition = 4;
+    static int savedArrowCounter = gameSpeed;
+    static int savedDinoCounter = 0;
+
+    counter++;
+
+    currentSegment = currentSegment % 4 + 1;
+
+    displayArrow(arrowPosition, ARROW_HEIGHT[arrowHeight]);
+    displayDino(dinoPos);
+
+    //dino recieves the information that it's supposed to move
+    if (dinoMoveFlag != 1){
+      //dino is in the neutral position and wants to jump
+      if (dinoMoveFlag == 0 && dinoPos == 1){
+        savedDinoCounter = counter;
+        dinoPos = 0;
+      }
+      //dino is in the neutral position and wants to duck
+      if (dinoMoveFlag == 2 && dinoPos == 1){
+        savedDinoCounter = counter;
+        dinoPos = 2;
+      }
+      //dino is not in the default pos. check if enough time has passed for it to return to default position
+      if (dinoMoveFlag == dinoPos ) {
+        if (counter - savedDinoCounter == gameSpeed * 2) {
+          dinoPos = 1;
+          dinoMoveFlag = 1;
+          savedDinoCounter = 0; //redundant?
+        }
+      }
+
+    }
+    
+    //update arrow
+    if (counter - savedArrowCounter == gameSpeed){
+      printf("Arrow: ");
+      //this is where we shoot the new arrow
+      if (arrowPosition == 1){
+        arrowHeight = rand()%3;
+        arrowPosition = 4;
+        printf("new arrow on %d\n", arrowPosition);
+      } else {
+        arrowPosition--;
+        printf("arrow moved at %d\n", arrowPosition);
+      }
+      //relative time of when was the last time the arrow was moved
+      savedArrowCounter = counter;
+    }
+    
+}
+
+
 int main(){
   initUSART();
   initDisplay();
   printf("START\n");
-  
+  initTimer0();
   setupButtons();
-  sei();
+  srand(0);
+  enableAllLeds();
 
-  while (1){
-    shootArrow();
-    
+
+  int lives = 4;
+  while (lives > 0){
+    sei();
+    lightUpMultipleLeds(lives);
+
   }
   
   return 0;
